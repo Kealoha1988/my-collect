0fa4c5fa92c0a0adb48e3c72004f3444351e0c47
Kealoha1988 <kealohaabe1988@gmail.com>
2020-11-12 15:59:36 -0500

    commit tree 7402d7aed9c44a2369c3eca635599c3976d05ba8

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(stuff)
+    i = 0
+    how_does_this_work = []
+    while i < stuff.length
+        how_does_this_work.push(yield(stuff[i]))
+        i = i + 1
+    end
+    how_does_this_work
+end
 
